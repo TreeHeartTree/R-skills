0bde140823fc36c7c4924135d0446e6a5c90276c
Lance Lin <linzhengzhong@icloud.com>
2015-12-09 14:09:15 -0800

    Logistic regression model applied..

--- class_prac.R
+++ class_prac.R
@@ -5,3 +5,41 @@ summary(Smarket)
 cor(Smarket[, -9])
 attach(Smarket)
 plot(Volume)
+head(Smarket)
+# Logistic regression model
+lr.fit <- glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume,
+              data = Smarket, family = binomial)
+summary(lr.fit)
+coef(lr.fit)
+summary(lr.fit)$coef[, 4] # p-values for the coef
+glm.probs <- predict(lr.fit, type = "response") #predict the class using predict()
+glm.probs[1:10]
+contrasts(Direction)
+glm.pred <- rep("Down", 1250)
+glm.pred[glm.probs > 0.5] = "Up"
+table(glm.pred, Direction)
+# split into train and test sets
+train = (Year < 2005)
+Smarket.2005 <- Smarket[!train, ]
+dim(Smarket.2005)
+Direction.2005 <- Direction[!train]
+glm.fit2 <- glm(Direction ~ Lag1 + Lag2 + Lag3 +Lag4 + Lag5 + Volume,
+                data = Smarket, family = binomial, subset = train)
+glm.probs <- predict(glm.fit2, Smarket.2005, type = "response")
+glm.pred <- rep("Down", dim(Smarket.2005)[1])
+glm.pred[glm.probs > 0.5] = "Up"
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005) # 48% accurate
+mean(glm.pred != Direction.2005) # 52% test error rate
+
+# cutting back the predictors to only Lag 1 & 2
+glm.fit3 <- glm(Direction ~ Lag1 + Lag2, data = Smarket,
+                family = binomial, subset = train)
+glm.probs3 <- predict(glm.fit3, Smarket.2005, type = "response") 
+glm.pred3 <- rep("Down", dim(Smarket.2005)[1])
+glm.pred3[glm.probs3 > 0.5] = "Up"
+table(glm.pred3, Direction.2005)
+mean(glm.pred3 == Direction.2005)
+mean(glm.pred3 != Direction.2005)
+# predict additional data points
+predict(glm.fit3, newdata = data.frame(Lag1 = c(1.2, 1.5), Lag2 = c(1.1, -0.8)), type = "response")
